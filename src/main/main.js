2529a11b72b659534d3ae982f2143e584ad58631
niinoutm <n.tmyk12@gmail.com>
2020-07-08 21:17:58 +0900

    設定入力→タイマー開始、実装

--- src/main/main.js
+++ src/main/main.js
@@ -1,9 +1,10 @@
-
-const SET_BEAT_ANIMATION = "1s ease 0s infinite normal none running beat";
-
+// テキスト
+const TEXT_SET_BEAT_ANIMATION = "1s ease 0s infinite normal none running beat";
 const TEXT_PLUS = "+";
 const TEXT_MINUS = "-";
+const TEXT_S = "s";
 
+// クラステキスト
 const CL_MINUS = "minus";
 const CL_PLUS = "plus";
 const CL_TD = "txt-deact";
@@ -11,15 +12,28 @@ const CL_TTD = "task-txt-deact";
 const CL_VT = "valid-txt";
 const CL_N = "none";
 
+// カラークラスリスト
+const CL_BD_LIST = ["cl-bd-0", "cl-bd-1", "cl-bd-2"];
+const CL_LIST = ["cl-0", "cl-1", "cl-2"];
+
+// タスクのリスト
 var taskList = [];
+// インターバル
 var interval = 0;
+// スコープ
+var scope = 0;
+// タスク数
+var taskCount = 2;
+
+// タイマー処理
+var timer;
 
 /** 初期処理 */
-$(function() {
-});
+$(function() {});
 
 /**
  * トグルボタンクリックイベント
+ * @returns void
  */
 function tglClickEvent() {
     if ($("#tglBtn").hasClass(CL_PLUS)) {
@@ -48,6 +62,7 @@ function tglClickEvent() {
 
 /**
  * セットボタンクリックイベント
+ * @returns void
  */
 function setclickEvent() {
     // 入力チェック
@@ -61,11 +76,13 @@ function setclickEvent() {
         $("#dialogWindow").addClass(CL_N);
         // 入力情報セット
         setTimerInfo();
+        startTimer();
     }
 }
 
 /**
  * 入力チェック
+ * @returns boolean result
  */
 function checkInput() {
     // クラスリセット
@@ -82,7 +99,11 @@ function checkInput() {
         result = false;
     }
     // タスク3(対象かどうかもチェック)
-    if (!$("#taskText3").hasClass(CL_TTD)
+    var isTask3 = !$("#taskText3").hasClass(CL_TTD);
+    if (isTask3) {
+        taskCount = 3;
+    }
+    if (isTask3
         && $("#taskText3").val() == "" ) {
         $("#taskText3").addClass(CL_VT);
         result = false; 
@@ -108,6 +129,73 @@ function checkInput() {
 
 /**
  * タイマー情報をセット
+ * @returns void
  */
 function setTimerInfo() {
+    // インターバル設定
+    resetTimerInfo();
+    // タスク名設定
+    $("#infoText").val(taskList[scope]);
+    // サークル色設定
+    $("#circleShadow").addClass(CL_BD_LIST[scope]);
+    // カウントテキスト設定
+    $("#circle").val(interval + TEXT_S);
+    // サークルアニメーション設定
+    $("#circleShadow").css("animation", TEXT_SET_BEAT_ANIMATION);
+}
+
+/**
+ * タイマー情報をリセット
+ * @returns void
+ */
+function resetTimerInfo() {
+    // サークルアニメーションをクリア
+    $("#circleShadow").css("animation", "");
+    // インターバルクリア
+    clearTimer();
+}
+
+/**
+ * タイマー開始
+ * @returns void
+ */
+function startTimer() {
+    // インターバル
+    timer = setInterval(function() {
+        // カウント取得
+        var count = $("#circle").val().split(TEXT_S)[0];
+        // カウント減算
+        count -= 1;
+        // カウント設定
+        $("#circle").val(count + TEXT_S);
+        if (count == 0) {
+            // カウントが0の場合
+            // サークル色設定
+            $("#circleShadow").removeClass(CL_BD_LIST[scope]);
+            // カウント色設定
+            $("#circle").removeClass(CL_LIST[scope]);
+            // scope加算
+            scope += 1;
+            if (scope == taskCount) {
+                // scopeがタスク数より多い場合scope初期化
+                scope = 0;
+            }
+            // タスク名設定
+            $("#infoText").val(taskList[scope]);
+            // カウント設定
+            $("#circle").val(interval + TEXT_S);
+            // サークル色設定
+            $("#circleShadow").addClass(CL_BD_LIST[scope]);
+            // カウント色設定
+            $("#circle").addClass(CL_LIST[scope]);
+        }
+    }, 1000);
+}
+
+/**
+ * タイマークリア
+ * @returns void
+ */
+function clearTimer() {
+    clearInterval(timer);
 }
